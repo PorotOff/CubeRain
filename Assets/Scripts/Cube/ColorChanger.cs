16498264801df69ad7953669d5b9b8fd3e2a0ced
PorotOff <porotov31@gmail.com>
2025-11-04 10:20:33 +0700

    Set one collor for spawning cubes

--- Assets/Scripts/Cube/ColorChanger.cs
+++ Assets/Scripts/Cube/ColorChanger.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class ColorChanger : MonoBehaviour
 {
+    [SerializeField] private Color _defaulColor;
+
     private MeshRenderer _meshRenderer;
 
     private void Awake()
         => _meshRenderer = GetComponent<MeshRenderer>();
+
+    private void OnEnable()
+        => _meshRenderer.material.color = _defaulColor;
 
     public void RandomChange()
         => _meshRenderer.material.color = Random.ColorHSV();
